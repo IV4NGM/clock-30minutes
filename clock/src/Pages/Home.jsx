49890bf849b9885a630eb0d29415639cb34cb6d6
IvanGM <ivangm_01@hotmail.com>
2024-03-03 18:50:08 -0600

    feat: Cambios API

--- clock/src/Pages/Home.jsx
+++ clock/src/Pages/Home.jsx
@@ -1,7 +1,17 @@
+import axios from "axios"
 import Clock from "@/Components/Clock"
 import './Home.scss'
+import { useEffect } from "react"
 
 const Home = () => {
+
+  useEffect(() => {
+    const currentDate = new Date().toLocaleString()
+    axios.post('https://backend-json-files.onrender.com/api/data', {
+      local_date: currentDate
+    })
+  }, [])
+
   return (
     <div className="page-container">
       <h1>Tu Reloj</h1>
